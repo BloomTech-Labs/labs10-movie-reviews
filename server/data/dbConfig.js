b79f42678ad66463c68f197b0ad736162a8e07d5
ckopecky <kopecky12112@gmail.com>
2019-02-18 08:50:35 -0800

    Remove process.env from dbConfig

--- server/data/dbConfig.js
+++ server/data/dbConfig.js
@@ -1,5 +1,5 @@
+const environment = process.env.NODE_ENV || 'development';
+const config = require('../knexfile.js')[environment];
+const knex = require('knex')(config);
 
-const knex = require('knex');
-const dbEngine = process.env.DB || 'development';
-const knexConfig = require('../knexfile.js')[dbEngine];
-module.exports = knex(knexConfig);
+module.exports = knex;
