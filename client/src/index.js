3e233d9b9ae203611138fd6f7ee72db0a0235b0d
Dil Sombayeva <dilyasom@gmail.com>
2019-02-18 10:57:29 -0800

    remove some packages

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 import UserReducer from './reducers';
-import 'bootstrap/dist/css/bootstrap.css';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
