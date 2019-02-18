e220ec39770a5b7ac718327c961b61e165d3cbf1
ckopecky <kopecky12112@gmail.com>
2019-02-18 15:06:40 -0800

    Add Landing Route to React-Router

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import Landing from './components/Landing';
 import SingleMovieView from './components/SingleMovieView';
 
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
         
         </Header> */}
         <Route exact path="/" component={Home} />
+        <Route path="/landing" component={Landing} />
         {/* <Route exact path="/movie/get/:id" component={SingleMovieView} /> */}
       </div>
     );
