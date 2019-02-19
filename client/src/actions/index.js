2d8f90a4c0409747108aadc1bcfd82be8249050c
Dil Sombayeva <dilyasom@gmail.com>
2019-02-19 15:05:43 -0800

    add import bootstrap

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -7,6 +7,7 @@ export const ERROR = 'ERROR';
 export const ADD_USER = 'ADD_USER';
 export const DELETED = 'DELETED';
 export const UPDATED = 'UPDATED';
+const baseURL = 'https://api.themoviedb.org/3/';
 
 export const gettingMovies = pageNum => {
   const promise = axios.get(
@@ -26,6 +27,26 @@ export const gettingMovies = pageNum => {
       });
   };
 };
+
+export const gettingRandomMovie = pageNum => {
+  const promise = axios.get(
+    `https://api.themoviedb.org/3/discover/movie?api_key=${
+      process.env.REACT_APP_API
+    }&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
+  );
+  console.log('Request in random: ', promise);
+  return dispatch => {
+    dispatch({ type: FETCHING_MOVIES });
+    promise
+      .then(response => {
+        dispatch({ type: FETCHED_MOVIES, payload: response.data });
+      })
+      .catch(err => {
+        dispatch({ type: ERROR, payload: err });
+      });
+  };
+};
+
 export const gettingMovieId = id => {
   console.log('id: ', id);
   const promise = axios.get(
