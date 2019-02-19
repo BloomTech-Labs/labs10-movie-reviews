f29cc0c9e9192ed8dab775205b76d4cc1656d59e
ckopecky <kopecky12112@gmail.com>
2019-02-19 15:03:09 -0800

    Add Prettier

--- server/api/reviews/reviewsHelper.js
+++ server/api/reviews/reviewsHelper.js
@@ -4,28 +4,28 @@ const db = require('../../data/dbConfig.js');
 // REVIEW HELPERS
 // ==============================================
 module.exports = {
-    //GET Reviews
-    getReviews: function(id) {
-        let query = db('movieReviews');
-        if (id) query.where('id', Number(id)).first();
-        return query;
-    },
-    //POST Review
-    insert: function(review) {
-        return db('movieReviews')
-        .insert(review)
-        .then(ids => ({ id: ids[0] }));
-    },
-    //PUT Review
-    update: function(id, movieReview) {
-        return db('movieReviews')
-            .where('id', id)
-            .update(movieReview);
-        },
-    //DELETE Review
-    remove: function(id) {
-        return db('movieReviews')
-            .where('id', id)
-            .del();
-    }
+  //GET Reviews
+  getReviews: function(id) {
+    let query = db('movieReviews');
+    if (id) query.where('id', Number(id)).first();
+    return query;
+  },
+  //POST Review
+  insert: function(review) {
+    return db('movieReviews')
+      .insert(review)
+      .then(ids => ({ id: ids[0] }));
+  },
+  //PUT Review
+  update: function(id, movieReview) {
+    return db('movieReviews')
+      .where('id', id)
+      .update(movieReview);
+  },
+  //DELETE Review
+  remove: function(id) {
+    return db('movieReviews')
+      .where('id', id)
+      .del();
+  }
 };
--- server/api/reviews/reviewsRouter.js
+++ server/api/reviews/reviewsRouter.js
@@ -9,59 +9,70 @@ const reviewsDb = require('./reviewsHelper.js');
 
 // GET request that returns all reviews from the database
 router.get('/reviews', async (req, res) => {
-    try {
-        const reviews = await reviewsDb.getReviews();
-        res.status(200).json(reviews);
-    } catch (err) {
-        res.status(500).json(err);
-    }
+  try {
+    const reviews = await reviewsDb.getReviews();
+    res.status(200).json(reviews);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //POST review
 router.post('/reviews', async (req, res) => {
-    if (req.body.twitterhandle && req.body.textBody && req.body.rating) {
-        try {
-        await reviewsDb.insert(req.body);
-        res.status(201).json({ message: 'Review successfully added!' });
-        } catch (err) {
-        res.status(500).json(err);
-        }
-    } else
-        res.status(400).json({
-        error: 'Please provide a username, rating and textBody for your review.'
-        });
+  if (req.body.twitterhandle && req.body.textBody && req.body.rating) {
+    try {
+      await reviewsDb.insert(req.body);
+      res.status(201).json({ message: 'Review successfully added!' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else
+    res.status(400).json({
+      error: 'Please provide a username, rating and textBody for your review.'
+    });
 });
 
 //PUT review
 router.put('/reviews/:id', async (req, res) => {
-    const changes = req.body;
+  const changes = req.body;
+  const { id } = req.params;
+  if (req.body.twitterhandle && req.body.textBody && req.body.rating) {
+    try {
+      const count = await reviewsDb.update(id, changes);
+      //count is the number of records updated
+      if (count) {
+        const review = await reviewsDb.get(id);
+        res.status(200).json(review);
+      } else
+        res.status(404).json({ Error: `Review with ID ${id} does not exist.` });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else
+    res
+      .status(400)
+      .json({
+        Error:
+          'Please provide a twitterhandle, textBody & rating to edit review.'
+      });
+});
+
+//DELETE request that deletes a review
+router.delete('/reviews/:id', async (req, res) => {
+  try {
     const { id } = req.params;
-    if (req.body.twitterhandle && req.body.textBody && req.body.rating) {
-        try {
-            const count = await reviewsDb.update(id, changes);        
-            //count is the number of records updated
-            if (count) {
-            const review = await reviewsDb.get(id);
-            res.status(200).json(review);
-            } else res.status(404).json({ Error: `Review with ID ${id} does not exist.` });
-        } catch (err) {
-            res.status(500).json(err);
-        }
-        } else res.status(400).json({ Error: 'Please provide a twitterhandle, textBody & rating to edit review.' });
-    });
-    
-    //DELETE request that deletes a review
-    router.delete('/reviews/:id', async (req, res) => {
-        try {
-        const { id } = req.params;
-        const count = await reviewsDb.remove(id);
-    
-        count
-            ? res.status(200).json({ message: `Review ${id} was successfully deleted.` })
-            : res.status(404).json({ message: `ID: ${id} does not exist in our database` });
-        } catch (err) {
-        res.status(500).json(err);
-        }
+    const count = await reviewsDb.remove(id);
+
+    count
+      ? res
+          .status(200)
+          .json({ message: `Review ${id} was successfully deleted.` })
+      : res
+          .status(404)
+          .json({ message: `ID: ${id} does not exist in our database` });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
