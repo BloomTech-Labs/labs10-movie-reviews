bf40b25cc648328d8a0752d919aa27abc7c6c00b
samsisle <samko9522@gmail.com>
2019-02-19 03:40:18 -0800

    Update production config variables

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,6 @@
 module.exports = {
-  dbURL: process.env.DATABASE_URL
-}
+  dbURL: process.env.DATABASE_URL,
+  twitterConsumerKey: process.env.TWITTER_CONSUMER_KEY,
+  twitterConsumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+  cookieKey: process.env.COOKIE_KEY
+};
