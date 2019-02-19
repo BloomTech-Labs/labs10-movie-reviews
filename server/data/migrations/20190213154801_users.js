a3bf3a1ef884a75cf540debd1879c26592ab6616
samsisle <samko9522@gmail.com>
2019-02-19 03:41:24 -0800

    Update users migration table

--- server/data/migrations/20190213154801_users.js
+++ server/data/migrations/20190213154801_users.js
@@ -1,12 +1,10 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', users => {
     users.increments(); // primary key called id
-    users
-      .string('username', 20)
-      .notNullable()
-      .unique(); // username field
-    users.string('name', 100).notNullable(); // name field
-    users.string('email', 254).notNullable(); // email field
+    users.string('twitterId', 25).unique(); // official twitterId of Twitter user
+    users.string('username', 20).unique(); // username field
+    users.string('name', 100); // name field
+    users.string('email', 254); // email field
     users.timestamp('created_at').defaultTo(knex.fn.now()); // user creation date
     users.timestamp('updated_at').defaultTo(knex.fn.now()); // last updated
   });
