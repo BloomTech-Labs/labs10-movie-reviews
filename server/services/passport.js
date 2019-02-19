4b9742d31dca95abcc190879f4909b349eaca4dd
samsisle <samko9522@gmail.com>
2019-02-19 03:43:27 -0800

    Add passport-twitter service

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,1 +1,36 @@
 const passport = require('passport');
+const TwitterStrategy = require('passport-twitter').Strategy;
+
+const usersDb = require('../api/authentication/authHelper.js');
+const keys = require('../config/keys.js');
+
+// passport-twitter strategy
+// ==============================================
+passport.serializeUser((user, done) => done(null, user.id));
+
+passport.deserializeUser(async (id, done) => {
+  const user = await usersDb.findUserById(id);
+  done(null, user);
+});
+
+passport.use(
+  new TwitterStrategy(
+    {
+      consumerKey: keys.twitterConsumerKey,
+      consumerSecret: keys.twitterConsumerSecret,
+      callbackURL: '/auth/twitter/callback',
+      proxy: true
+    },
+    async (token, tokenSecret, profile, done) => {
+      const existingUser = await usersDb.findUserByTwitterId({
+        twitterId: profile.id
+      });
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        const user = await usersDb.createUser({ twitterId: profile.id });
+        done(null, user);
+      }
+    }
+  )
+);
