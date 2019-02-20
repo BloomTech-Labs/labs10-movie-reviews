fd9ba835845953171ba7798933df9fd13deb5edc
ckopecky <kopecky12112@gmail.com>
2019-02-20 09:47:41 -0800

    Refactor Hero Element Header into its own component

---
+++ client/src/components/home/Hero/HeroHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Hero.css";
+
+const HeroHeader = (props) => {
+    return (
+        <>
+          <h1 className="app-name">{props.appName}</h1>
+          <div className="app-subtitle">{props.appSlug}</div>
+        </>
+    );
+};
+
+export default HeroHeader;
