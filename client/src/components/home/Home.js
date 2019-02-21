b556230552cb559d9cefbaa28232fad7552f48fe
ckopecky <kopecky12112@gmail.com>
2019-02-21 07:45:02 -0800

    Change home and hero to presentational components

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,46 +1,25 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import MappedItem from './MappedItem';
 
-import axios from 'axios';
 import './mappedItem.css';
 import { Card } from 'reactstrap';
+import Hero from './Hero/Hero';
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movies: [],
-      loading: true
-    };
-  }
-  componentDidMount() {
-    const promise = axios.get(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${
-        process.env.REACT_APP_API
-      }&language=en-US&page=1`
-    );
-    promise
-      .then(response => {
-        console.log('response: ', response);
-        this.setState({
-          movies: response.data.results,
-          loading: false
-        });
-        console.log('movies: ', this.state.movies);
-        //sets the information retrieved onto state
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
-  render() {
-    console.log('props in home page: ', this.props);
-    const { loading } = this.state;
-    if (loading) {
-      return null;
-    }
+
+const Home = (props) => {
     return (
+      <>
+      <Hero 
+        loading={props.loading} 
+        random={props.randomBackgroundImage}
+        randomTitle={props.randomTitle}
+        getReleaseYear={props.getReleaseYear}
+        handleChange={props.handleChange}
+        searchHandler={props.searchHandler}
+        searchResults={props.searchResults}
+        searchCriteria={props.searchCriteria}
+      
+      />
       <div className="home-page">
         <h1>Welcome back, User!</h1>
         <div className="featured">
@@ -52,8 +31,11 @@ class Home extends Component {
         <div className="featured">
           <h2>Popular Movies</h2>
           <div className="cardWrapper">
-            {this.state.movies.map(item => {
-              return <MappedItem key={item.id} item={item} />;
+            {props.movies.map(item => {
+              return <MappedItem 
+                key={item.id} 
+                item={item} 
+              />;
             })}
           </div>
         </div>
@@ -64,8 +46,8 @@ class Home extends Component {
           </Card>
         </div>
       </div>
+    </>
     );
-  }
-}
+  };
 
 export default Home;
