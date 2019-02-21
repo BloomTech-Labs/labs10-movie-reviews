fc03341590c53e5bb59ccc2b4391cdcf721a3400
ckopecky <kopecky12112@gmail.com>
2019-02-21 09:06:51 -0800

    Change Hero into Presentational Component

--- client/src/components/home/Hero/Hero.js
+++ client/src/components/home/Hero/Hero.js
@@ -1,31 +1,29 @@
 import React from 'react';
 import HeroElement from './HeroElement';
 
-
 import './Hero.css';
 
-const Hero = (props) => {
+const Hero = props => {
   return (
-      <div
-        className="hero"
-        style={{ backgroundImage: 'url(' + props.random + ')' }}
-      >
-        {/* used inline style to create background image since it is on state */}
-        <div className="header-overlay">
-          
-            <HeroElement 
-              className={props.resultLength > 0 ? "search-result-header" : "hidden"}
-              searchHandler={props.searchHandler}
-              getReleaseYear={props.getReleaseYear}
-              handleChange={props.handleChange}
-              randomTitle={props.randomTitle}
-              resultLength={props.resultLength}
-              searchCriteria={props.searchCriteria}
-              searchResults={props.searchResults}
-            />
-          </div>
-        </div>
-    );
-  }
+    <div
+      className="hero"
+      style={{ backgroundImage: 'url(' + props.random + ')' }}
+    >
+      {/* used inline style to create background image since it is on state */}
+      <div className="header-overlay">
+        <HeroElement
+          className={props.resultLength > 0 ? 'search-result-header' : 'hidden'}
+          searchHandler={props.searchHandler}
+          getReleaseYear={props.getReleaseYear}
+          handleChange={props.handleChange}
+          randomTitle={props.randomTitle}
+          resultLength={props.resultLength}
+          searchCriteria={props.searchCriteria}
+          searchResults={props.searchResults}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Hero;
