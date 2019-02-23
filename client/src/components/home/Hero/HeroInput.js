1dc2b87033faa0a9692c1cbd5f9bd399ef10834c
ckopecky <kopecky12112@gmail.com>
2019-02-23 10:14:52 -0800

    Add styling to hero input to make it disappear when going mobile.

--- client/src/components/home/Hero/HeroInput.js
+++ client/src/components/home/Hero/HeroInput.js
@@ -3,17 +3,17 @@ import './Hero.css';
 
 const HeroInput = (props) => {
     return (
-        <>
-        <label className="landing-label">{props.label}</label>
-        <input
-            onChange={props.handleChange}
-            className="landing-input"
-            type="text"
-            name="searchCriteria"
-            value={props.searchCriteria}
-            placeholder={props.randomTitle}
-        />
-        </>
+        <div className="hero-input">
+            <label className="landing-label">{props.label}</label>
+            <input
+                onChange={props.handleChange}
+                className="landing-input"
+                type="text"
+                name="searchCriteria"
+                value={props.searchCriteria}
+                placeholder={props.randomTitle}
+            />
+        </div>
     );
 };
 
