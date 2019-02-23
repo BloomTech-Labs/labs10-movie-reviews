7615644e4e29f6455496fabf4fdb6074afde97b1
ckopecky <kopecky12112@gmail.com>
2019-02-22 22:47:35 -0800

    Add Searcg to NavBar

---
+++ client/src/components/navbar/NavSearch.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const NavSearch = (props) => {
+    return (
+        <div className="input-and-button">
+            <input
+                onChange={props.handleChange}
+                className="nav-input"
+                type="text"
+                name="searchCriteria"
+                value={props.searchCriteria}
+                placeholder="Search..."
+            />
+            <div className="call-to-action-buttons">
+                <div className="button-nav" onClick={props.searchHandler}>
+                    <Link to={`/search/?q=${props.searchCriteria}`}>{<i class="fas fa-search"></i>}</Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NavSearch;
