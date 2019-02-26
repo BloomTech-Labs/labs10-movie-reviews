3485e08ced55efb7facd3f50ec8934b123e767cd
mparedes003 <onnabugeishadev@gmail.com>
2019-02-26 14:12:15 -0800

    DELETE works. Create function that sets reviw id to this.id for use in deleting. Add handleDelete Event handler with axios call that allows for deletion and updating of state. Add delete button to review

--- client/src/components/myReviews/Review.js
+++ client/src/components/myReviews/Review.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import axios from 'axios';
+
 class Review extends Component {
   state = {
     review: null,
@@ -10,6 +12,33 @@ class Review extends Component {
     textBody: ''
   };
 
+  // sets review id to this.id for use in deleting
+  get id() {
+    return this.props.review.id;
+  }
+
+  // allows us to delete and update state
+  handleDelete = e => {
+    e.preventDefault();
+    axios
+      .delete(`http://localhost:5000/api/reviews/${this.id}`)
+      .then(response => {
+        this.props.fetchReviews();
+        this.setState({
+          review: response.data,
+          userid: response.data.userId,
+          movieId: response.data.movieId,
+          twitterhandle: response.data.twitterhandle,
+          rating: response.data.rating,
+          textBody: response.data.textBody
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    window.location.reload();
+  };
+
   render() {
     const { rating, textBody } = this.props.review;
 
@@ -17,6 +46,9 @@ class Review extends Component {
       <div>
         <p>rating: {rating}</p>
         <p>textbody: {textBody}</p>
+        <button className="delete-edit-btn" onClick={this.handleDelete}>
+          Delete
+        </button>
       </div>
     );
   }
