016992ec162eb1350c6cce11790b83c88b6b5ec0
ckopecky <kopecky12112@gmail.com>
2019-02-26 12:52:43 -0800

    Add photo to user table

--- server/data/migrations/20190213154801_users.js
+++ server/data/migrations/20190213154801_users.js
@@ -2,12 +2,13 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', users => {
     users.increments(); // primary key called id
     users.string('twitterId', 25).unique(); // official twitterId of Twitter user
-    users.string('googleId', 50).unique();
-    users.string('stripeId', 50).unique();
+    users.string('googleId', 50).unique();//official googleId of google user
+    users.string('stripeId', 50).unique();//official stripeId of stripe user
     users.string('username', 20).unique(); // username field
     users.string('name', 100); // name field
     users.string('email', 254); // email field
     users.boolean('premium_user').defaultTo(false);
+    users.string('photo',100);
     users.timestamp('created_at').defaultTo(knex.fn.now()); // user creation date
     users.timestamp('updated_at').defaultTo(knex.fn.now()); // last updated
   });
