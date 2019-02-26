062c54336fb1bca8f22aefe61f5948e190004242
ari7946 <mercado7946@gmail.com>
2019-02-26 13:51:04 -0800

    Broke down into smaller components. Hardcoded payment information for security in the checkout

--- client/src/components/premium/PayButton.js
+++ client/src/components/premium/PayButton.js
@@ -5,51 +5,50 @@ import axios from "axios";
 const PayButton = props => {
   const publishableKey = "pk_test_GAKbu7bXAJ5UZjNKNbWEe0XF";
 
-  // Ontoken sends information about the card to Stripe.
-  // Then it receives a token object that contains details about the transaction.
-  // (card type, last 4 digit It returns a token object from Stripe which
-  // contains information about the transaction (card type, last 4 digits, email, ect)s, email, ect)
-  const onToken = token => {
+  function onToken(token) {
     console.log("token", token)
-    console.log("props.header", props.header)
-    // the body object contains our token from Stripe, 
-    // the total amount of cents, and the props that specify 
-    // the type of subscription, yearly or monthly
     const body = {
-        amount: props.totalCents,
-        token: token,
-        plan: props.header,
-    };
+      amount: props.totalCents,
+      token: token,
+      plan: props.header,
+  };
 
-    // we send the body object to our backend with an axios request.
-    // Our backend will then create a new customer and a new subscription.
-    axios
-      .post("http://localhost:5000/api/payment", body)
-      .then(response => {
-        console.log("response", response);
-      })
-      .catch(error => {
-        console.log("Payment Error: ", error);
-      });
+  axios
+    .post("http://localhost:5000/api/payment", body)
+    .then(response => {
+      console.log("response", response);
+    })
+    .catch(error => {
+      console.log("Payment Error: ", error);
+    });
   };
 
   return (
-    <StripeCheckout // component comes from react-stripe-checkout package
-      yearly={props.header}
-      label={props.buttonText} //Component button text
-      name={props.header} //Modal Header
-      description={props.priceTitle}
-      panelLabel="Submit Payment" //Submit button in modal
-      amount={props.totalCents} //Amount in cents 
-
-      // ontoken is invoked here.
-      token={res => onToken(res)} 
-      stripeKey={publishableKey}
-      // image="" //Pop-in header image
-      billingAddress={false}
-    >
-      <button className="btn border">Pay Now</button>
-    </StripeCheckout>
+    props.header === "Yearly Subscription" ? (
+      <StripeCheckout
+        label={"Pay Now"} //Component button text
+        name={"Yearly Subscription"} //Modal Header
+        description={"$9.99"}
+        panelLabel="Submit Payment" //Submit button in modal
+        amount={999} //Amount in cents 
+        token={res => onToken(res)}
+        stripeKey={publishableKey}
+        // image="" //Pop-in header image
+        billingAddress={false}
+      />
+    ) : (
+      <StripeCheckout
+        label={"Pay Now"} //Component button text
+        name={"Monthly Subscription"} //Modal Header
+        description={"$0.99"}
+        panelLabel="Submit Payment" //Submit button in modal
+        amount={99} //Amount in cents 
+        token={res => onToken(res)}
+        stripeKey={publishableKey}
+        // image="" //Pop-in header image
+        billingAddress={false}
+      />
+    )
   );
 };
 
