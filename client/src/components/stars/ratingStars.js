a1a72f6362357bd99777c914d792c07332672ce0
Dil Sombayeva <dilyasom@gmail.com>
2019-02-28 08:54:23 -0800

    star forlder

---
+++ client/src/components/stars/ratingStars.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import StarRatingComponent from 'react-star-rating-component';
+import './stars.css';
+
+export default class ratingStars extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      rating: 0
+    };
+  }
+
+  onStarClick(nextValue, prevValue, name) {
+    this.setState({ rating: nextValue }, () => {
+      this.setState({ rating: this.state.rating });
+      console.log('stars num: ', this.state.rating);
+    });
+  }
+  componentWillReceiveProps(nextProps) {
+    this.setState({ rating: [...this.state.rating, nextProps.addToList] }); // notice the difference this.props vs nextProps
+  }
+
+  render() {
+    const { rating } = this.state;
+
+    return (
+      <div>
+        <h2>Rating from state: {rating}</h2>
+        <StarRatingComponent
+          name="rate1"
+          starCount={5}
+          value={rating}
+          onStarClick={this.onStarClick.bind(this)}
+        />
+      </div>
+    );
+  }
+}
