eb1dcda8516f6cd6ce932a98fa47b4fedebf76b0
ckopecky <kopecky12112@gmail.com>
2019-02-27 16:16:12 -0800

    Add link to movie profile page with reviews

--- client/src/components/home/Hero/SearchResults.js
+++ client/src/components/home/Hero/SearchResults.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ResultCard from './ResultCard';
+import { Link } from 'react-router-dom';
 
 const SearchResults = props => {
     return (
@@ -8,12 +9,14 @@ const SearchResults = props => {
           return (
             //saving index on result-card so that index is available to query movie details information
             //TODO: Query movie details information so that each movie can have it's own profile page.
-            <ResultCard
-              result={result}
-              releaseYear={props.getReleaseYear}
-              index={result.id}
-              key={result.id}
-            />
+            <Link to={`/moviereviews/${result.id}`} className="linksMovie">
+              <ResultCard
+                result={result}
+                releaseYear={props.getReleaseYear}
+                index={result.id}
+                key={result.id}
+              />
+            </Link>
           );
         })}
       </div>
