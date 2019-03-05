d5e84e4b458af3a68b1df5eb9d9cfced0c29d4ca
ari7946 <mercado7946@gmail.com>
2019-03-05 13:03:02 -0800

    display information about subs. Conditionally renders content and css based on current subscription

--- client/src/components/premium/PremiumCard.js
+++ client/src/components/premium/PremiumCard.js
@@ -3,20 +3,51 @@ import './premium.css';
 import PayButton from "./PayButton";
 
 const premiumCard = (props) => {
-
+  console.log('props current sub', props.currentSub)
   return (
-    <div className="card mb-3">
-      <div className="card-header">
+    <div 
+      className={`
+        card 
+        mb-3 
+        rounded
+        bg-white
+        ${!props.premium ? 'shadow-lg' : null}  
+        ${props.currentSub === 'Yearly' 
+          ? 'border border-info shadow-lg' 
+          : 'bg-light'}
+        ${props.currentSub === 'Monthly' 
+          ? 'border border-info shadow-lg' 
+          : 'bg-light'}
+      `}
+    >
+      <div className="card-header bg-dark text-white">
         {props.header}
       </div>
 
-      <div className="card-body">
-        <h5 className="card-title text-success">{props.priceTitle}</h5>
-        <p className="card-text">{props.description}</p>
-        <PayButton 
-          header={props.header} 
-          totalCents={props.totalCents}  
-        />
+      <div className="card-body bg-light">
+        <h3 className="card-title text-success">
+          <span 
+            className={`
+              badge 
+              ${props.currentSub === 'Yearly' 
+                ? 'badge-info' 
+                : 'badge-secondary'}
+              ${props.currentSub === 'Monthly' 
+                ? 'badge-info' 
+                : 'badge-secondary'}
+            `}
+          >
+            {props.priceTitle}
+          </span>
+        </h3>
+        <p className="card-text text-dark">{props.description}</p>
+        {props.displayNone 
+          ? null
+          : <PayButton 
+              header={props.header} 
+              totalCents={props.totalCents}  
+            />
+        }
       </div>
     </div>
   )
