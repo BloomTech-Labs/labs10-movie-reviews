b991ab8e626c7dca3f9f1875b72f10300e3eb913
Dil Sombayeva <dilyasom@gmail.com>
2019-03-05 13:43:10 -0800

    change router

--- server/api/users/usersRouter.js
+++ server/api/users/usersRouter.js
@@ -9,33 +9,30 @@ const usersDb = require('./usersHelper.js');
 
 // A GET request that returns all users from the database
 router.get('/users', async (req, res) => {
-  try {
-    const users = await usersDb.getUsers();
-    if (debugging === true) console.log('GET Users Router:', users);
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  const users = await usersDb.getUsers();
+  res.json(users);
 });
 
 // GET request that gets a user by id
 router.get('/users/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const user = await usersDb.getUsers(id);
-    if (debugging === true) console.log('GET User By ID Router:', user);
-    user
-      ? res.status(200).json(user)
-      : res.status(404).json({ error: 'The user with the specified ID does not exist'})
-  } catch (err) {
-    res.status(500).json(err)
-  }
-})
+  const { id } = req.params;
+  const user = await usersDb.getUsers(id);
+  res.json(user);
+});
 
 // POST request to add a user
 router.post('/users', async (req, res) => {
   if (req.body.name && req.body.username && req.body.email) {
-    if (debugging === true) console.log('POST User Router', '\nname:', req.body.name, 'username:', req.body.username, 'email:', req.body.email);
+    if (debugging === true)
+      console.log(
+        'POST User Router',
+        '\nname:',
+        req.body.name,
+        'username:',
+        req.body.username,
+        'email:',
+        req.body.email
+      );
     try {
       await usersDb.insert(req.body);
       res.status(201).json({ message: 'User successfully added!' });
@@ -52,18 +49,22 @@ router.post('/users', async (req, res) => {
 router.put('/users/:id', async (req, res) => {
   const changes = req.body;
   const { id } = req.params;
-if (req.body.name && req.body.email && req.body.stripeId) {
+  if (req.body.name && req.body.email && req.body.stripeId) {
     try {
-      const count = await usersDb.update(id, changes);        
+      const count = await usersDb.update(id, changes);
       //count is the number of records updated
       if (count) {
         const user = await usersDb.get(id);
         res.status(200).json(user);
-      } else res.status(404).json({ Error: `User with ID ${id} does not exist.` });
+      } else
+        res.status(404).json({ Error: `User with ID ${id} does not exist.` });
     } catch (err) {
       res.status(500).json(err);
     }
-  } else res.status(400).json({ Error: 'Please provide a name, username & email for the user profile.' });
+  } else
+    res.status(400).json({
+      Error: 'Please provide a name, username & email for the user profile.'
+    });
 });
 
 //A DELETE request that deletes a user
