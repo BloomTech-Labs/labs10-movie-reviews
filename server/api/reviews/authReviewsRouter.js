eb18243ebee7cfcad9401a6a1273669cc968497e
mparedes003 <onnabugeishadev@gmail.com>
2019-03-07 14:07:42 -0800

    Refactor PUT reviews route

--- server/api/reviews/authReviewsRouter.js
+++ server/api/reviews/authReviewsRouter.js
@@ -12,7 +12,14 @@ const reviewsDb = require('./reviewsHelper.js');
 //POST review
 router.post('/reviews', async (req, res) => {
   if (req.body.textBody && req.body.rating) {
-    if (debugging === true) console.log('POST Router', '\ntextBody:', req.body.textBody, 'rating:', req.body.rating);
+    if (debugging === true)
+      console.log(
+        'POST Router',
+        '\ntextBody:',
+        req.body.textBody,
+        'rating:',
+        req.body.rating
+      );
 
     try {
       await reviewsDb.insert(req.body);
@@ -28,27 +35,31 @@ router.post('/reviews', async (req, res) => {
 
 //PUT review
 router.put('/reviews/:id', async (req, res) => {
-  const changes = req.body;
   const { id } = req.params;
+
+  const editedReview = await reviewsDb.update(req.params.id, req.body);
+  const review = await reviewsDb.get(id);
+
   if (req.body.textBody && req.body.rating) {
-    if (debugging === true) console.log('PUT Router', '\ntextBody:', req.body.textBody, 'rating:', req.body.rating);
     try {
-      const count = await reviewsDb.update(id, changes);
-      //count is the number of records updated
-      if (count) {
-        if (debugging === true) console.log('PUT Count:', count);
-        const review = await reviewsDb.get(id);
-        if (debugging === true) console.log('PUT Review:', review);
-        res.status(200).json(review);
-      } else
-        res.status(404).json({ Error: `Review with ID ${id} does not exist.` });
-    } catch (err) {
-      res.status(500).json(err);
+      // make sure edited review is not null
+      if (editedReview === 0) {
+        // if it is then send 404
+        return res
+          .status(404)
+          .json({ Error: `Review with ID ${id} does not exist.` });
+      } else {
+        // otherwise send a 200 on success
+        return res.status(200).json(review);
+      }
+    } catch (error) {
+      // catch any other error and send a 500
+      return res.status(500).json({
+        message: 'the review could not be edited',
+        error: error.message
+      });
     }
-  } else
-    res.status(400).json({
-      Error: 'Please provide a name, textBody & rating to edit review.'
-    });
+  }
 });
 
 //DELETE request that deletes a review
