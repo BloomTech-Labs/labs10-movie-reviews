3517388d686517db05e02691b6729b93a20a1df2
mparedes003 <onnabugeishadev@gmail.com>
2019-03-07 08:38:21 -0800

    Add code for delete modal

--- client/src/components/myReviews/Review.js
+++ client/src/components/myReviews/Review.js
@@ -101,6 +101,50 @@ class Review extends Component {
 
     return (
       <div className="card mb-2 box-shadow mb-3 shadow p-2 mb-5 bg-white">
+        <div
+          class="modal fade"
+          id="exampleModal"
+          tabindex="-1"
+          role="dialog"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">
+                  Modal title
+                </h5>
+                <button
+                  type="button"
+                  class="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">...</div>
+              <div class="modal-footer">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  data-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-primary"
+                  onClick={this.handleDelete}
+                >
+                  Save changes
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <Row>
           <Col sm="4">
             <Link to={`/moviereviews/${this.props.review.movieId}`}>
@@ -149,8 +193,9 @@ class Review extends Component {
                   </button>
                   <button
                     type="button"
-                    className="btn btn-outline-danger"
-                    onClick={this.handleDelete}
+                    class="btn btn-primary"
+                    data-toggle="modal"
+                    data-target="#exampleModal"
                   >
                     Delete
                   </button>
