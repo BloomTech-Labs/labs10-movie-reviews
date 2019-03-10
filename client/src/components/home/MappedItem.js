e1743f58f15f73abc81fd1e7f037c1bfe5232dd8
ari7946 <mercado7946@gmail.com>
2019-03-10 15:37:26 -0700

    remove commented out code

--- client/src/components/home/MappedItem.js
+++ client/src/components/home/MappedItem.js
@@ -7,28 +7,6 @@ import { tmdbUrl } from '../../services/resourceURLs';
 const MappedItem = props => {
   return (
     <Col lg={3} md={4}>
-<<<<<<< HEAD
-      <Link to={`moviereviews/${props.item.id}`} className="linksMovie" style={{ textDecoration: 'none' }}>
-        <div>
-          <img 
-            src={`http://image.tmdb.org/t/p/original//${props.item.backdrop_path}`} className="card-img img-responsive" alt="movie image" />
-          <p className="movie-title container"><span className="title-text rounded">{props.item.title}</span></p>
-        </div>
-=======
-      {/* <div className="singleMovie">
-        <Card>
-          <Link to={`moviereviews/${props.item.id}`} className="linksMovie">
-            <CardImg
-              src={`${tmdbUrl}//${
-                props.item.backdrop_path
-              }`}
-              alt="image"
-            />
-            <CardTitle>{props.item.title}</CardTitle>
-            <hr />
-          </Link>
-        </Card>
-      </div> */}
       <Link
         to={`moviereviews/${props.item.id}`}
         className="linksMovie"
@@ -42,7 +20,6 @@ const MappedItem = props => {
         <p className="movie-title">
           <span className="title-text rounded small">{props.item.title}</span>
         </p>
->>>>>>> 50845d202444e19c8532532ab1cec68a23369360
       </Link>
     </Col>
   );
