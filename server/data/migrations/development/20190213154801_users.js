911dbcef7e281377abc5b77bafff65c1eeddb172
mparedes003 <onnabugeishadev@gmail.com>
2019-03-11 09:56:08 -0700

    Add review order field

--- server/data/migrations/development/20190213154801_users.js
+++ server/data/migrations/development/20190213154801_users.js
@@ -2,13 +2,14 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', users => {
     users.increments('id'); // primary key called id
     users.string('twitterId', 25).unique(); // official twitterId of Twitter user
-    users.string('googleId', 50).unique();//official googleId of google user
-    users.string('stripeId', 50).unique();//official stripeId of stripe user
+    users.string('googleId', 50).unique(); //official googleId of google user
+    users.string('stripeId', 50).unique(); //official stripeId of stripe user
     users.string('username', 20).unique(); // username field
     users.string('name', 100); // name field
     users.string('email', 254).unique(); // email field
     users.boolean('premium_user').defaultTo(false);
-    users.string('photo',200);
+    users.string('photo', 200);
+    users.text('reviewOrder').notNullable(); // review order field
     users.timestamp('created_at').defaultTo(knex.fn.now()); // user creation date
     users.timestamp('updated_at').defaultTo(knex.fn.now()); // last updated
   });
