f3c873aa514af06a06e7b5e846c6f65a80f0356b
mparedes003 <onnabugeishadev@gmail.com>
2019-03-11 11:15:29 -0700

    Refactor GET route

--- server/api/reviews/reviewsRouter.js
+++ server/api/reviews/reviewsRouter.js
@@ -12,10 +12,25 @@ const reviewsDb = require('./reviewsHelper.js');
 // GET request that returns all reviews from the database
 router.get('/reviews', async (req, res) => {
   // get all reviews
-  const allReviews = await reviewsDb.getReviews();
+  const allReviews = await reviewsDb.getReviews().orderBy('id', 'desc');
   try {
-    // return all reviews to the caller
-    return res.status(200).json(allReviews);
+    // setup the order string
+    const reviewOrderString = await users.getReviewOrder(1);
+
+    // parse the json and extract the reviewOrderArray
+    const reviewOrderArray = JSON.parse(reviewOrderString.reviewOrder);
+    // check there is a note order array
+    if (reviewOrderArray.length === 0) {
+      return res.status(200).json(allReviews);
+    } else {
+      // set an orderedNotes by mapping over the array and ordering via a find
+      const orderedReviews = reviewOrderArray.map(order => {
+        return allReviews.find(review => review.id === order);
+      });
+      // return all reviews to the caller
+      // return res.status(200).json(allReviews);
+      return res.status(200).json(orderedReviews);
+    }
   } catch (error) {
     // catch any error left and send it to the caller
     return res.status(500).json({
