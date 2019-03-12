a28983a4b5d30a81748012c605199db4e6f47e5a
mparedes003 <onnabugeishadev@gmail.com>
2019-03-12 13:48:07 -0700

    Made modal class for delete modal

---
+++ client/src/components/myReviews/Modal.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './modal.css';
+
+const Modal = props => {
+  return (
+    <div
+      className="modal-wrapper"
+      style={{
+        top: props.show ? '15%' : '-100%',
+        opacity: props.show ? '1' : '0'
+      }}
+    >
+      <div className="modal-header">
+        <h3>Are you sure?</h3>
+      </div>
+      <div className="modal-body">
+        <div>
+          <p>Do you really want to delete this review?</p>
+        </div>
+      </div>
+      <div className="modal-footer">
+        <button className="btn-cancel" onClick={props.close}>
+          Cancel
+        </button>
+        <button className="btn-delete" onClick={props.handleDelete}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Modal;
