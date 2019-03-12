97ca0ad5f98ae6862ea5cfb7c91580960309dbac
ckopecky <kopecky12112@gmail.com>
2019-03-12 13:21:59 -0700

    Change app subtitle div to h4

--- client/src/components/home/Hero/HeroHeader.js
+++ client/src/components/home/Hero/HeroHeader.js
@@ -5,7 +5,7 @@ const HeroHeader = (props) => {
     return (
         <>
           <h1 className="app-name">{props.appName}</h1>
-          <div className="app-subtitle">{props.appSlug}</div>
+          <h4 className="app-subtitle">{props.appSlug}</h4>
         </>
     );
 };
