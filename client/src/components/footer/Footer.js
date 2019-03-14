65e544c929983c06bfbf4c178288ff981d0533bb
ckopecky <kopecky12112@gmail.com>
2019-03-13 17:32:26 -0700

    Added mobile functionality to footer

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -11,20 +11,37 @@ const Footer = props => {
           <Link to="/about"><h5>About</h5></Link>
           <Link to="/contact"><h5>Contact</h5></Link>
         </div>
-        <p className="footer-text">
-        <Link to="/" className="home">{`© CineView. `}</Link>{`Made by fans in the United States.  `}
-        <Link className="footer-link" to="/about">{`Film data `}</Link>
+        <p className="footer-text mobile-hidden">
+          <Link to="/" className="home">{`© CineView. `}</Link>{`Made by fans in the United States.  `}
+          <Link className="footer-link" to="/about">{`Film data `}</Link>
+          {`from `}
+          <a href={`${theMovieDbUrl}`} className="footer-link">
+          {`TMDb`}
+          </a>
+          .
+        </p>
+        <div className="links">
+          <Link to="/privacy"><h5>Privacy</h5></Link>
+          <Link to="/terms"><h5>Terms</h5></Link>
+        </div>
+      </nav>
+      <p className="mobile-display hidden-desktop">
+        <Link 
+          to="/" 
+          className="mobile-home">
+          {`© CineView. `}
+        </Link>
+        <Link 
+          className="footer-link-mobile" 
+          to="/about">
+          {`Film data `}
+        </Link>
         {`from `}
-        <a href={`${theMovieDbUrl}`} className="footer-link">
+        <a 
+          href={`${theMovieDbUrl}`} className="footer-link-mobile">
           {`TMDb`}
         </a>
-        .
-      </p>
-        <div className="links">
-        <Link to="/privacy"><h5>Privacy</h5></Link>
-        <Link to="/terms"><h5>Terms</h5></Link>
-      </div>
-      </nav>
+      .</p>
     </div>
   );
 };
