d3024a199e785c4744b1a16650b7ddc196f47515
ckopecky <kopecky12112@gmail.com>
2019-03-15 10:14:18 -0700

    Change server to PROD all the time

--- client/src/services/userURLs.js
+++ client/src/services/userURLs.js
@@ -1,9 +1,9 @@
 export const users =
   process.env.NODE_ENV === 'production'
     ? `${process.env.REACT_APP_PROD_SERVER_URI}/api/users`
-    : `${process.env.REACT_APP_DEV_SERVER_URI}/api/users`;
+    : `${process.env.REACT_APP_PROD_SERVER_URI}/api/users`;
 
 export const currentUser =
   process.env.NODE_ENV === 'production'
     ? `${process.env.REACT_APP_PROD_SERVER_URI}/auth/current_user`
-    : `${process.env.REACT_APP_DEV_SERVER_URI}/auth/current_user`;
+    : `${process.env.REACT_APP_PROD_SERVER_URI}/auth/current_user`;
