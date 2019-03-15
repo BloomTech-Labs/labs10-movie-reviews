0618c96340c4c2ff680d0d8fe4fdde4cc81d1376
Charletta Bullard <charlettabullard12@outlook.com>
2019-03-15 13:59:08 -0400

    Revert "Rewire so localhost always talks to deployed backend. "

--- client/src/services/paymentURLs.js
+++ client/src/services/paymentURLs.js
@@ -1,14 +1,14 @@
 export const customerDelete =
   process.env.NODE_ENV === 'production'
     ? `${process.env.REACT_APP_PROD_SERVER_URI}/api/customer/delete`
-    : `${process.env.REACT_APP_PROD_SERVER_URI}/api/customer/delete`;
+    : `${process.env.REACT_APP_DEV_SERVER_URI}/api/customer/delete`;
 
 export const customerPlan =
   process.env.NODE_ENV === 'production'
     ? `${process.env.REACT_APP_PROD_SERVER_URI}/api/customer/plan`
-    : `${process.env.REACT_APP_PROD_SERVER_URI}/api/customer/plan`;
+    : `${process.env.REACT_APP_DEV_SERVER_URI}/api/customer/plan`;
 
 export const makePayment =
   process.env.NODE_ENV === 'production'
     ? `${process.env.REACT_APP_PROD_SERVER_URI}/api/payment`
-    : `${process.env.REACT_APP_PROD_SERVER_URI}/api/payment`;
+    : `${process.env.REACT_APP_DEV_SERVER_URI}/api/payment`;
