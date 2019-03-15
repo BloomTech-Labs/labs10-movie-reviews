cdd9e46cda1714a3858b9fefd5af8f4c0b04b8fe
Charletta Bullard <charlettabullard12@outlook.com>
2019-03-15 14:04:53 -0400

    Revert "Rewire so localhost always talks to deployed backend. "

--- client/src/services/authURLs.js
+++ client/src/services/authURLs.js
@@ -1,14 +1,14 @@
 export const twitterLogin =
   process.env.NODE_ENV === 'production'
     ? `${process.env.REACT_APP_PROD_SERVER_URI}/auth/twitter`
-    : `${process.env.REACT_APP_PROD_SERVER_URI}/auth/twitter`
+    : `${process.env.REACT_APP_DEV_SERVER_URI}/auth/twitter`
 
 export const logout =
   process.env.NODE_ENV === 'production'
     ? `${process.env.REACT_APP_PROD_SERVER_URI}/auth/logout`
-    : `${process.env.REACT_APP_PROD_SERVER_URI}/auth/logout`
+    : `${process.env.REACT_APP_DEV_SERVER_URI}/auth/logout`
 
 export const googleLogin =
   process.env.NODE_ENV === 'production'
     ? `${process.env.REACT_APP_PROD_SERVER_URI}/auth/google`
-    : `${process.env.REACT_APP_PROD_SERVER_URI}/auth/google`
+    : `${process.env.REACT_APP_DEV_SERVER_URI}/auth/google`
