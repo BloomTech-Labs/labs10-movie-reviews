0ac39a7f73c3688d6c3e48f525dcba8364da1d9a
ckopecky <kopecky12112@gmail.com>
2019-03-18 13:46:40 -0700

    Add comment to explain why it's commented out

--- client/src/services/authURLs.js
+++ client/src/services/authURLs.js
@@ -9,6 +9,7 @@ export const googleLogin =
     ? `${process.env.REACT_APP_PROD_SERVER_URI}/auth/google`
     : `${process.env.REACT_APP_DEV_SERVER_URI}/auth/google`
 
+// comment this out as we are not using it. 
 // export const twitterLogin =
 //   process.env.NODE_ENV === 'production'
 //     ? `${process.env.REACT_APP_PROD_SERVER_URI}/auth/twitter`
