3eebfa3c528cca533913cb411b45a51461114cbf
ckopecky <kopecky12112@gmail.com>
2019-03-18 08:30:46 -0700

    Take out Twitter login

--- client/src/components/navbar/Modal.js
+++ client/src/components/navbar/Modal.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import GoogleLogin from '../../assets/svg/btn_google_1.png';
-import TwitterLogin from '../../assets/svg/btn_twitter_1.svg';
-import { googleLogin, twitterLogin } from '../../services/authURLs';
+import { googleLogin } from '../../services/authURLs';
 import "./Navbar.css";
 
 import RenderLogin from './RenderLogin';
@@ -19,9 +18,6 @@ const LoginModal = props => {
           <ModalBody>
               <RenderLogin>
                 <a href={googleLogin}><img src={GoogleLogin} alt="Log In to Google"/></a>
-              </RenderLogin>
-              <RenderLogin>
-                <a href={twitterLogin}><img src={TwitterLogin} alt="Log In to Twitter"/></a>
               </RenderLogin>
           </ModalBody>
         </Modal>
