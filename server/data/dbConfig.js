501b3649c078eddcb6d7ce69dc161d9687a96f9f
ckopecky <kopecky12112@gmail.com>
2019-03-19 13:48:48 -0700

    Take out console.log

--- server/data/dbConfig.js
+++ server/data/dbConfig.js
@@ -1,5 +1,4 @@
 const environment = process.env.NODE_ENV || 'development';
 const config = require('../knexfile.js')[environment];
 const knex = require('knex')(config);
-console.log('config: ', config);
 module.exports = knex;
